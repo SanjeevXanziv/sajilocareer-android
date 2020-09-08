d516aeb50867b231955657e964171be815eba6cb
Suman Bakhati <kindsumansh@gmail.com>
2020-09-08 08:23:49 +0545

    manifest and widgets explaination

---
+++ app/src/main/java/com/application/androidmanifest/MyActivity.kt
@@ -0,0 +1,11 @@
+package com.application.androidmanifest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my)
+    }
+}
