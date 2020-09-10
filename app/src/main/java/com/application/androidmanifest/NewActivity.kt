bc4f2c6efe25266247d59cca8f02b745182705e7
Suman Bakhati <kindsumansh@gmail.com>
2020-09-10 09:55:31 +0545

    we study gradle scripts

--- app/src/main/java/com/application/androidmanifest/NewActivity.kt
+++ app/src/main/java/com/application/androidmanifest/NewActivity.kt
@@ -2,11 +2,35 @@ package com.application.androidmanifest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class NewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new)
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.profile, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        val id = item.itemId
+        if (id == R.id.setting){
+           /*val myToast =  Toast.makeText(this, "This is Setting",Toast.LENGTH_SHORT)
+            myToast.show()*/
+            Toast.makeText(this, "This is Setting",Toast.LENGTH_SHORT).show()
+
+        }
+
+
+
+
+        return super.onOptionsItemSelected(item)
     }
 }
